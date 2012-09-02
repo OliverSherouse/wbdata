c75d55ef9c136d48ced5349e30626d8806fa5f75
Oliver Sherouse <oliver.sherouse@gmail.com>
2012-09-02 00:31:57 -0400

    refactored MASSIVE FUNCTION and added preliminary pandas support

--- wbdata.py
+++ wbdata.py
@@ -1,21 +1,21 @@
 """
 wbdata: A wrapper for the World Bank API
-
-Copyright (C) 2012  Oliver Sherouse <oliver DOT sherouse AT gmail DOT com>
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not. If not, see <http://www.gnu.org/licenses/>.
 """
+
+#Copyright (C) 2012  Oliver Sherouse <Oliver DOT Sherouse AT gmail DOT com>
+
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program; if not. If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function, unicode_literals
 
@@ -26,6 +26,11 @@ import urllib2
 import json
 import time
 
+try:
+    import pandas as pd
+except ImportError:
+    pd = None
+
 
 BASE_URL = "http://api.worldbank.org"
 COUNTRIES_URL = "{0}/countries".format(BASE_URL)
@@ -33,25 +38,25 @@ ILEVEL_URL = "{0}/incomeLevels".format(BASE_URL)
 INDICATOR_URL = "{0}/indicator".format(BASE_URL)
 LTYPE_URL = "{0}/lendingTypes".format(BASE_URL)
 SOURCES_URL = "{0}/sources".format(BASE_URL)
-TOPIC_URL = "{0}/topic".format(BASE_URL)
+TOPIC_URL = "{0}/topics".format(BASE_URL)
 PER_PAGE = 1000
 INDIC_ERROR = "Cannot specify more than one of indicator, source, and topic"
 
-def __parse_string_or_iterable(arg):
+
+def __parse_value_or_iterable(arg):
     """
-    Return string if arg is a string or ';'-delimited string if arg is a
-    sequence
-    
-    :returns: string
+    :arg: either a single object or a sequence of objects
+    :returns: a string with either the single value or list joined by ';'
     """
     if isinstance(arg, basestring):
-        return arg
+        return str(arg)
     return ";".join(arg)
+
 
 def __get_paged_data(query_url, args=None):
     """
     Page through queries and return a list of the results
-    
+
     :query_url: the pageless query url
     :returns: the merged list of all results from all pages
     """
@@ -78,10 +83,48 @@ def __get_paged_data(query_url, args=None):
     return results
 
 
+def __parse_monthly_date(data_date):
+    return data_date.strftime("%YM%m")
 
-def get_data_for_indicator(indicator, countries="all", aggregates=None,
-                           data_date=None, mrv=None, gapfill=None,
-                           frequency="Y"):
+
+def __parse_quarterly_date(data_date):
+    return "{0}Q{1}".format(data_date.year, data_date.month // 4)
+
+
+def __parse_yearly_date(data_date):
+    return data_date.strftime("%Y")
+
+
+def __parse_data_date(frequency, data_date):
+    if isinstance(data_date, datetime.date):
+        try:
+            return {"M": __parse_monthly_date,
+                    "Q": __parse_quarterly_date,
+                    "Y": __parse_yearly_date}[frequency](data_date)
+        except KeyError:
+            raise ValueError("Bad Frequency")
+    elif len(data_date) == 2:
+        try:
+            parser = {"M": __parse_monthly_date,
+                      "Q": __parse_quarterly_date,
+                      "Y": __parse_yearly_date}[frequency]
+            return ":".join((parser(data_date[0]), parser(data_date[1])))
+        except KeyError:
+            raise ValueError("Bad Frequency")
+    raise TypeError("Bad data_date")
+
+
+def __convert_to_dataframe(data, column_name):
+    if not pd:
+        raise ValueError("Pandas must be installed to be used")
+    return pd.DataFrame({"country": [i["country"] for i in data],
+                         "date": [i["date"] for i in data],
+                         column_name: [i["value"] for i in data]})
+
+
+def get_data(indicator, countries="all", aggregates=None, data_date=None,
+             mrv=None, gapfill=None, frequency="Y", pandas=True,
+             column_name="value"):
     """
     Retrieve indicators for given countries and years
 
@@ -89,7 +132,12 @@ def get_data_for_indicator(indicator, countries="all", aggregates=None,
     :countries: a country code, sequence of country codes, or "all" (default)
     :aggregates: the regional or aggregate code, or sequence thereof
     :date: the desired date as a datetime.date object or a 2-sequence with
-    start and end dates, or None
+        start and end dates
+    :mrv: the number of most recent values to retrieve
+    :gapfill: with mrv, fills gaps with the most recent values
+    :frequency: 'Q', 'M', or 'Y' for quarterly, monthly, or annual data
+    :pandas: if true, return results as a pandas dataframe
+    :column_name: the desired name for the pandas column
     :returns: @todo
     """
     query_url = COUNTRIES_URL
@@ -97,51 +145,31 @@ def get_data_for_indicator(indicator, countries="all", aggregates=None,
         if countries != "all":
             raise ValueError("Cannot Specify both countries and aggregates")
         try:
-            c_part = __parse_string_or_iterable(aggregates)
+            c_part = __parse_value_or_iterable(aggregates)
         except TypeError:
-            raise TypeError("'aggregates' must be a string, iterable, or None")
+            raise TypeError("aggregates must be a string, iterable, or None")
     else:
         try:
-            c_part = __parse_string_or_iterable(countries)
+            c_part = __parse_value_or_iterable(countries)
         except TypeError:
             raise TypeError("'countries' must be a string or iterable'")
     query_url = "/".join((query_url, c_part, "indicators", indicator))
     args = []
     if data_date:
-        if isinstance(data_date, datetime.date):
-            if frequency == "M":
-                d_part = data_date.strftime("%YM%m")
-            elif frequency == "Y":
-                d_part = data_date.strftime("%Y")
-            else:
-                raise ValueError("Bad Frequency")
-        elif len(data_date) == 2:
-            if frequency == "M":
-                d_part = ":".join((data_date[0].strftime("%YM%m"),
-                                   data_date[1].strftime("%YM%m")))
-            elif frequency == "Y":
-                d_part = ":".join((data_date[0].strftime("%Y"),
-                                   data_date[1].strftime("%Y")))
-            elif frequency == "Q":
-                d_part = "{0}Q{1}:{2}Q{3}".format(data_date[0].year,
-                                                  data_date[0].month // 4,
-                                                  data_date[1].month,
-                                                  data_date[1].month // 4)
-            else:
-                raise ValueError("Bad Frequency")
-        else:
-            raise TypeError("Bad data_date")
-        args.append(("date", d_part))
+        args.append(("date", __parse_data_date(frequency, data_date)))
     if mrv:
         args.append(("MRV", mrv))
     if gapfill:
         args.append(("Gapfill", "Y"))
-    return __get_paged_data(query_url)
+    data = __get_paged_data(query_url, args)
+    if pandas:
+        return __convert_to_dataframe(data, column_name)
+    return data
 
 
 def __id_only_query(query_url, id_or_ids):
     """@todo: Docstring for __id_only_query
-    
+
     :query_url: @todo
     :id_or_ids: @todo
     :returns: @todo
@@ -158,7 +186,7 @@ def __id_only_query(query_url, id_or_ids):
 def get_source(source_id=None):
     """
     Retrieve information on a source
-    
+
     :source_id: an id number or sequence thereof.  None returns all sources.
     :returns: @todo
     """
@@ -167,16 +195,25 @@ def get_source(source_id=None):
 
 def get_incomelevel(level_id=None):
     """@todo: Docstring for get_incomelevel
-    
+
     :level_id: @todo
     :returns: @todo
     """
     return __id_only_query(ILEVEL_URL, level_id)
 
 
+def get_topic(topic_id=None):
+    """@todo: Docstring for get_topic
+
+    :topic_id: @todo
+    :returns: @todo
+    """
+    return __id_only_query(TOPIC_URL, topic_id)
+
+
 def get_lendingtype(type_id=None):
     """@todo: Docstring for get_lendingtype
-    
+
     :level_id: @todo
     :returns: @todo
     """
@@ -184,24 +221,27 @@ def get_lendingtype(type_id=None):
 
 
 def get_indicator(indicator=None, source=None, topic=None):
-    """@todo: Docstring for get_indicator
-    
-    :indicator: @todo
-    :source: @todo
-    :topic: @todo
-    :returns: @todo
+    """
+    Retrieve information about an indicator or indicators.  Only one of
+    indicator, source, and topic can be specified.  Specifying none of the
+    three will return all indicators.
+
+    :indicator: the specific indicator code
+    :source: a source id
+    :topic: a topic id
+    :returns: a list of dictionary objects representing indicators
     """
     if indicator:
         if source or topic:
             raise ValueError(INDIC_ERROR)
-        query_url = "/".join(INDICATOR_URL, indicator)
+        query_url = "/".join((INDICATOR_URL, indicator))
         return __get_paged_data(query_url)
     if source:
         if topic:
             raise ValueError(INDIC_ERROR)
-        query_url = "/".join(SOURCES_URL, source, "indicators")
+        query_url = "/".join((SOURCES_URL, str(source), "indicators"))
         return __get_paged_data(query_url)
     if topic:
-        query_url = "/".join(TOPIC_URL, source, "indicators")
+        query_url = "/".join((TOPIC_URL, str(topic), "indicators"))
         return __get_paged_data(query_url)
     return __get_paged_data(INDICATOR_URL)
