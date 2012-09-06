97275d6983de267cc48b65ba808384ee573e5b2d
Oliver Sherouse <oliver.sherouse@gmail.com>
2012-09-05 22:11:39 -0400

    fixed url

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     author="Oliver Sherouse",
     author_email="oliver.sherouse@gmail.com",
     packages=["wbdata"],
-    url="http://pypi.python.org/pypi/wbdata",
+    url="http://code.google.com/p/wbdata/",
     licence="GPL2",
     description="A library to access World Bank data",
-    long_description=open('README.txt'.read()),
-    )
+    long_description=open('README.txt'.read())
+)
