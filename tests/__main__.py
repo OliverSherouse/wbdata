3a70f4cf0461bdb14224d49e320b160b55079cb7
Oliver Sherouse <oliver.sherouse@gmail.com>
2012-09-25 21:40:48 -0400

    Initial Testing Suite
    First commit of tests.  Right now a lot of them just check that routine
    checking does not throw errors; correctness isn't checked for in most cases.
    There's a TODO if I ever saw one.

---
+++ tests/__main__.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import json
+import logging
+import unittest
+
+import pandas
+import wbdata
+
+from tests import strings
+
+#logging.basicConfig(level=logging.DEBUG)
+
+
+class GetCountryTestCase(unittest.TestCase):
+    def testAllCountries(self):
+        data = wbdata.get_country()
+
+    def testUSA(self):
+        expected = json.loads(strings.USA_DATA)
+        got = wbdata.get_country(country_id="USA")
+        self.assertEqual(expected, got)
+
+    def testOEC(self):
+        expected = json.loads(strings.OEC_DATA)
+        got = wbdata.get_country(incomelevel="OEC")
+        self.assertEqual(expected, got)
+
+    def testOEC_NOC(self):
+        expected = json.loads(strings.OEC_DATA)
+        got = wbdata.get_country(incomelevel="OEC")
+        self.assertEqual(expected, got)
+
+    def testIDB(self):
+        expected = json.loads(strings.IDB_DATA)
+        got = wbdata.get_country(lendingtype="IDB")
+        self.assertEqual(expected, got)
+
+    @unittest.expectedFailure
+    def testBadCountry(self):
+        wbdata.get_country(country_id="Foobar")
+
+    @unittest.expectedFailure
+    def testBadIncomeLevel(self):
+        wbdata.get_country(incomelevel="Foobar")
+
+    @unittest.expectedFailure
+    def testBadLendingType(self):
+        wbdata.get_country(incomelevel="Foobar")
+
+
+class TestGetIndicator(unittest.TestCase):
+    def testGetAllIndicators(self):
+        wbdata.get_indicator()
+
+    def testGetOneIndicator(self):
+        wbdata.get_indicator("SH.XPD.PRIV.ZS")
+
+    def testGetIndicatorBySource(self):
+        wbdata.get_indicator(source=1)
+
+    def testGetIndicatorByTopic(self):
+        wbdata.get_indicator(topic="1")
+
+    @unittest.expectedFailure
+    def testGetIndicatorBySourceAndTopic(self):
+        wbdata.get_indicator(source="1", topic=1)
+
+
+class TestSimpleQueries(unittest.TestCase):
+    def testGetAllIncomeLevels(self):
+        wbdata.get_incomelevel()
+
+    def testOneIncomeLevel(self):
+        wbdata.get_incomelevel("OEC")
+
+    def testGetAllLendingTypes(self):
+        wbdata.get_lendingtype()
+
+    def testOneIncomeLevel(self):
+        wbdata.get_lendingtype("IBD")
+
+    def testGetAllSources(self):
+        wbdata.get_source()
+
+    def testOneSource(self):
+        wbdata.get_source(31)
+
+    def testGetAllTopics(self):
+        wbdata.get_source()
+
+    def testOneTopic(self):
+        wbdata.get_source("3")
+
+if __name__ == '__main__':
+    unittest.main()
