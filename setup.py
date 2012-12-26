1bb4cdfa59bdaaa12f03960546617b9d4855dbb3
Oliver Sherouse <oliver.sherouse@gmail.com>
2012-12-26 23:14:58 +0100

    added decorator as a dependency

--- setup.py
+++ setup.py
@@ -8,8 +8,11 @@ setup(
     author="Oliver Sherouse",
     author_email="oliver.sherouse@gmail.com",
     packages=["wbdata"],
-    url="https://github.com/nofrak/wbdata",
+    url="https://github.com/oliversherouse/wbdata",
     licence="GPL2",
     description="A library to access World Bank data",
+    install_requires=[
+        "decorator >= 3.4.0"
+    ],
     long_description=open('README.rst').read()
 )
