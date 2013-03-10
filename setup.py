41ae56ec2e9dc204850e1fb2fae81c1387acfd58
Oliver Sherouse <oliver.sherouse@gmail.com>
2013-03-10 19:15:34 -0400

    bumped to version 0.2.0 for tag

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='wbdata',
-    version='0.1.0',
+    version='0.2.0',
     author="Oliver Sherouse",
     author_email="oliver.sherouse@gmail.com",
     packages=["wbdata"],
