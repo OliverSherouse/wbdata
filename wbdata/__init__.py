6cb03918f2ea9193292934c80dcc6d704e819d66
Oliver Sherouse <oliver@oliversherouse.com>
2017-08-23 18:04:35 -0400

    get rid of deprecated panel functionality and add get_series

--- wbdata/__init__.py
+++ wbdata/__init__.py
@@ -5,7 +5,7 @@ wbdata: A wrapper for the World Bank API
 from __future__ import (print_function, division, absolute_import,
                         unicode_literals)
 
-from .api import (get_country, get_data, get_dataframe, get_panel,
+from .api import (get_country, get_data, get_series, get_dataframe,
                   get_indicator, get_incomelevel, get_lendingtype, get_source,
                   get_topic, search_countries, search_indicators)
 
